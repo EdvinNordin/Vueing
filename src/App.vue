b55edda68f647eb894d3f1cd091bd2e622b83d12
Edvin Nordin <nordin.edvin@gmail.com>
2025-05-11 22:47:30 +0200

    Vueing around

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+  },
+};
+</script>
+
+<template>
+  <div class="text-center pb-10">
+    <h1 class="text-3xl font-bold underline">
+      Edvin Nordin</h1>
+  </div>
+  <div class="flex flex-row justify-between">
+    <div class="border w-1/3 text-center h-50">
+      <h3>Project 1</h3>
+    </div>
+    <div class="border w-1/3 text-center">
+      <h3>Project 2</h3>
+    </div>
+    <div class="border w-1/3 text-center">
+      <h3>Project 3</h3>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
