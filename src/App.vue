07b75bfbc5668583fe4471e654afdecc12819514
Edvin Nordin <nordin.edvin@gmail.com>
2025-05-11 23:32:46 +0200

    playground stuff

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,51 @@
 export default {
   data() {
     return {
+      projects: [
+        {
+          name: "Project 1",
+          description: "Water"
+        },
+        {
+          name: "Project 2",
+          description: "Game"
+        },
+        {
+          name: "Project 3",
+          description: "Tabs"
+        }
+      ],
     };
   },
 
   methods: {
+    mouseEnter(project) {
+      project.temp = project.description;
+      project.description = "hovering";
+      
+    },
+    mouseLeave(project) {
+      project.description = project.temp;
+    }
   },
 };
 </script>
 
 <template>
   <div class="text-center pb-10">
-    <h1 class="text-3xl font-bold underline">
-      Edvin Nordin</h1>
+    <h1 class="text-3xl font-bold underline">Edvin Nordin</h1>
   </div>
   <div class="flex flex-row justify-between">
-    <div class="border w-1/3 text-center h-50">
-      <h3>Project 1</h3>
-    </div>
-    <div class="border w-1/3 text-center">
-      <h3>Project 2</h3>
-    </div>
-    <div class="border w-1/3 text-center">
-      <h3>Project 3</h3>
+    <div class="border w-1/3 text-center h-50"
+    v-for="project in projects"
+    @mouseover="mouseEnter(project)"
+    @mouseleave="mouseLeave(project)">
+
+      <h3>{{project.name}}</h3>
+      <p>{{project.description}}</p>
+
     </div>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
