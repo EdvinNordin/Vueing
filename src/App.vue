c6207f6c89bf141bee0f3b757faf76dcca546f03
Edvin Nordin <nordin.edvin@gmail.com>
2025-05-19 13:22:33 +0000

    Refactor code structure for improved readability and maintainability

--- src/App.vue
+++ src/App.vue
@@ -20,31 +20,28 @@ export default {
   },
 
   methods: {
-    mouseEnter(project) {
-      project.temp = project.description;
-      project.description = "hovering";
-      
-    },
-    mouseLeave(project) {
-      project.description = project.temp;
+    clicking(){
+      alert("You clicked on a project!");
     }
-  },
+  }
 };
 </script>
 
 <template>
-  <div class="text-center pb-10">
-    <h1 class="text-3xl font-bold underline">Edvin Nordin</h1>
-  </div>
-  <div class="flex flex-row justify-between">
-    <div class="border w-1/3 text-center h-50"
-    v-for="project in projects"
-    @mouseover="mouseEnter(project)"
-    @mouseleave="mouseLeave(project)">
-
-      <h3>{{project.name}}</h3>
-      <p>{{project.description}}</p>
-
+  <div class="flex flex-col justify-center w-full h-screen max-w-full">
+    <div class="text-center pb-10">
+      <h1 class="text-3xl font-bold underline">Edvin Nordin</h1>
+    </div>
+    <div class="flex flex-row justify-between w-full px-2 md:px-10">
+      <div
+        class="border text-center h-50 w-50"
+        @click="clicking"
+        v-for="project in projects"
+        :key="project.name"
+      >
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+      </div>
     </div>
   </div>
 </template>
