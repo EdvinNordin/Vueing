b3bf0e42e03f8a7da9ab8c4253e2a9b01dd1b76a
Edvin Nordin <nordin.edvin@gmail.com>
2025-05-22 15:47:10 +0200

    hightlight correction

--- src/App.vue
+++ src/App.vue
@@ -84,20 +84,22 @@ export default {
     @mouseover="hovering" 
     @mouseleave="stopHovering" >
       <div v-for="project in projects" :key="project.name">
-        <a :href="project.link" target="_blank" class="level-item">
-        <div class="box level-item is-flex-direction-column is-justify-content-flex-start customSize mb-4" 
-        @mouseover="highlight(project)" 
-        @mouseleave="stopHighlight(project)"
-        :class="project.isHovering ? 'has-background-light' : 'has-background-wight'">
-          <div class="is-justify-content-space-between">
-            <p class="title is-5 customTitle">{{ project.name }}</p>
-
-            <img :src="project.image" :alt="project.name" class="image is-256x256 projImage"/>
+        <a :href="project.link" target="_blank">
+          <div class="box level-item is-flex-direction-column is-justify-content-space-between customSize" 
+           @mouseover="highlight(project)" @mouseleave="stopHighlight(project)"
+          :class="project.isHovering ? 'has-background-light' : 'has-background-wight'">
+            <div class="is-justify-content-flex-start">
+              <div class="is-justify-content-space-between">
+                <p class="title is-5 customTitle">{{ project.name }}</p>
+                <img :src="project.image" :alt="project.name" class="image projImage"/>
+              </div>
+              <p class="custom-max-width customDescription">{{ project.description }}</p>
+            </div>
+            <a :href="project.github" target="_blank" class="">
+              <img src="/github.png" class="image is-24x24 githubIcon" alt="Github icon"/>
+            </a>
           </div>
-          <p class="custom-max-width customDescription"> {{ project.description }}</p>
-        
-        </div>
-      </a>
+        </a>
       </div>
     </div>
   </section>
@@ -148,6 +150,13 @@ export default {
 .customDescription{
   white-space: normal; /* Allows text to wrap to the next line */
   padding-top:15px;
+}
+
+.githubIcon{
+  
+    position:absolute;
+    bottom:0;
+    right:0;
 }
 
 /*
